d63e2c853025d3cdd81327cca48e8e288e8547ac
Luca Pacitto <47114030+Nembie@users.noreply.github.com>
2023-05-15 14:48:01 +0200

    [Perf]

--- esso.py
+++ esso.py
@@ -3,6 +3,29 @@ import sys
 import zipfile
 from tqdm import tqdm
 
+def print_banner():
+    print(r"""
+      ___           ___           ___           ___     
+     /  /\         /  /\         /  /\         /  /\    
+    /  /::\       /  /::\       /  /::\       /  /::\   
+   /  /:/\:\     /__/:/\:\     /__/:/\:\     /  /:/\:\  
+  /  /::\ \:\   _\_ \:\ \:\   _\_ \:\ \:\   /  /:/  \:\ 
+ /__/:/\:\ \:\ /__/\ \:\ \:\ /__/\ \:\ \:\ /__/:/ \__\:\
+ \  \:\ \:\_\/ \  \:\ \:\_\/ \  \:\ \:\_\/ \  \:\ /  /:/
+  \  \:\ \:\    \  \:\_\:\    \  \:\_\:\    \  \:\  /:/ 
+   \  \:\_\/     \  \:\/:/     \  \:\/:/     \  \:\/:/  
+    \  \:\        \  \::/       \  \::/       \  \::/   
+     \__\/         \__\/         \__\/         \__\/    
+    """)
+    print("Esso - Easy and Simple Zip cracker")
+    print("Author: @Nembie")
+
+def print_help():
+    print("Usage: python esso.py -f <file_name> -w <wordlist_name>")
+    print("Options:")
+    print("-f <file_name>     Specify the name of the file to crack")
+    print("-w <wordlist_name> Specify the name of the wordlist to use")
+    print("-h                 Print this help")
 
 def extract_file(z_file, password):
     try:
@@ -11,38 +34,9 @@ def extract_file(z_file, password):
     except Exception:
         return None
 
-
-def main():
-    # If parameter -h is used, the help is shown
-    if len(sys.argv) > 1 and sys.argv[1] == "-h":
-        print("Usage: python3 esso.py -f <file_path> -w <wordlist>")
-        print("Example: python3 esso.py -f file.zip -w wordlist.txt")
-        exit(0)
-
-    # If parameter -f is used, the file name is the first parameter, otherwise it is asked to the user
-    if len(sys.argv) > 2 and sys.argv[1] == "-f":
-        file_name = sys.argv[2]
-    else:
-        file_name = input("Insert the name of the file: ")
-
-    # Get filename, If the filename doesn't exist, exit
-    if not os.path.exists(file_name):
-        print("[-] File not found.")
-        exit(0)
-
-    # If parameter -w is used, the wordlist name is the third parameter, otherwise it is asked to the user
-    if len(sys.argv) > 4 and sys.argv[3] == "-w":
-        word_list = sys.argv[4]
-    else:
-        word_list = input("Insert the name of the wordlist: ")
-
-    # Get wordlist, If the wordlist doesn't exist, exit
-    if not os.path.exists(word_list):
-        print("[-] Wordlist not found.")
-        exit(0)
-
+def job(file_name, word_list):
+    # Read all lines from the wordlist at once
     with open(word_list, mode="r", encoding="latin-1") as pass_file:
-        # Read all lines from the wordlist at once
         wordlist_lines = pass_file.readlines()
 
     # Open the protected file only once
@@ -63,12 +57,28 @@ def main():
 
     print("[-] Password not found.")
 
+def main():
+    # If parameter -f is used, the file name is the first parameter, otherwise it is asked to the user
+    file_name = (sys.argv[2] if len(sys.argv) > 2 and sys.argv[1] == "-f" else input("Insert the name of the file: "))
+    # If parameter -w is used, the wordlist name is the third parameter, otherwise it is asked to the user
+    word_list = (sys.argv[4] if len(sys.argv) > 4 and sys.argv[3] == "-w" else input("Insert the name of the wordlist: "))
+
+    # Get filename and wordlist, If the file or the wordlist doesn't exist, exit
+    if not os.path.exists(file_name) or not os.path.exists(word_list):
+        print("[-] File or wordlist not found.")
+        exit(0)
+
+    job(file_name, word_list)
 
 if __name__ == '__main__':
+    print_banner()
+    if len(sys.argv) > 1 and sys.argv[1] == "-h":
+        print_help()
+        exit(0)
     try:
         main()
     except KeyboardInterrupt:
-        print('\nInterrupted')
+        print("\n[-] Exiting...")
         try:
             sys.exit(0)
         except SystemExit:
